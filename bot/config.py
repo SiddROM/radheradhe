2bd991563d77d6d707714472b27dc00c42561ced
SiddROM <121415948+SiddROM@users.noreply.github.com>
2023-06-09 14:53:53 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -17,21 +17,21 @@ from decouple import config
 
 
 class Var:
-    REDIS_URI = config("REDIS_URI", default=None)
-    REDIS_PASS = config("REDIS_PASS", default=None)
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
+    REDIS_URI = config("redis://admin:7518@Sidd@redis://default:Pn2F48emzKB7TE1nDuUAVDG8yLSmTZuK@redis-17361.c305.ap-south-1-1.ec2.cloud.redislabs.com:17361/Disha-free-db", default=None)
+    REDIS_PASS = config("7518@Sidd", default=None)
+    API_ID = config("26019444", default=6, cast=int)
+    API_HASH = config("a308fac723905cdbd836414b18f3b3d6", default="")
+    BOT_TOKEN = config("6100344970:AAGrq-OnnEwz9KBahxtCcdo68pXG7kV6Z1I", default=None)
     BACKUP = config("BACKUP", default=0, cast=int)
     FFMPEG = config("FFMPEG", default="ffmpeg")
-    CHAT = config("CHAT", cast=int)
+    CHAT = config("-1001873702781", cast=int)
     THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/37d9d0657d51e01a71f26.jpg"
+        "THUMBNAIL", default="https://te.legra.ph/file/7489c3a0ff4002c01253b.jpg"
     )
-    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
-    CLOUD = config("CLOUD", cast=int)
+    LOG_CHANNEL = config("-1001970622682", default=0, cast=int)
+    CLOUD = config("-1001802196143", cast=int)
     GDRIVE_FOLDER_ID = config("GDRIVE_FOLDER_ID", default=None)
     TOKEN_FILE_LINK = config("TOKEN_FILE_LINK", default=None)
     INDEX_LINK = config("INDEX_LINK", default="https://github.com/")
-    OWNERS = config("OWNERS", default="")
+    OWNERS = config("6046532356", default="")
     GDRIVE_UPLOAD = config("GDRIVE_UPLOAD", default=False, cast=bool)
